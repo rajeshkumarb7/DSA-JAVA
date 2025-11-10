f97fcc1f3d1ff9a15255bc7c8b7ac967e4093bc8
rajeshkumarb7 <rajeshmadurai376@gmail.com>
2025-11-10 11:28:23 +0530

    sort methods o(n^2) is added

--- Sorting and Searching/DemoSort.java
+++ Sorting and Searching/DemoSort.java
@@ -30,11 +30,13 @@ public class DemoSort {
         System.out.println("After sort of arr :"+""+Arrays.toString(arr));
         Insertionsort.reverseSort(arr);
         System.out.println("After reversesort of arr :"+""+Arrays.toString(arr));
+        Insertionsort.Sort_no_swap(arr);
+        System.out.println("After sort of arr without using swap :"+""+Arrays.toString(arr));
         System.out.println("_______________________________________");
 
-        /* 
+        /*/
         arr= new int[]{1,324,53,13,5,823,34,75,9876,1,23,3,53,65,3,2,-23,-2,-3,-2223,-22,-32};
-        System.out.println("demo for Bubble sort");
+        System.out.println("demo for QUICK sort");
         System.out.println("before sort of arr :"+""+Arrays.toString(arr));
         SelectionSort.sort(arr);
         System.out.println("After sort of arr :"+""+Arrays.toString(arr));
--- Sorting and Searching/Insertionsort.java
+++ Sorting and Searching/Insertionsort.java
@@ -1,9 +1,55 @@
 public class Insertionsort {
+    // worst case O(n^2) Stable sort
+    // best case O(n)
     public static void sort(int[] arr) {
-        
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j-1]>arr[j]){
+
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
     }
     
     public static void reverseSort(int[] arr) {
-        
+                for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j-1]<arr[j]){
+
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
     } 
+
+
+    public static void Sort_no_swap(int[] arr) { // swap logic is used so better
+    for (int i = 1; i < arr.length; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        // Move elements of arr[0..i-1] that are greater than key
+         // to one position ahead of their current position
+        // shift larger elements to the right
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // insert key at correct position
+        arr[j + 1] = key;
+    }
+}
+
 }
